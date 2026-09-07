bcbbebf9fa2a6083dc69b4bffeb08836afbcb526
Evelyn Bandeca <57359349+evybandeca@users.noreply.github.com>
2026-09-07 20:07:01 -0300

    feat: carregar aprofundamento TL-11.1 de Macunaíma

--- js/corpus-registry.js
+++ js/corpus-registry.js
@@ -10,7 +10,7 @@
     'ursula':{scripts:['js/corpus/ursula.js']},
     'memorias-sargento-milicias':{scripts:['js/corpus/memorias-sargento-milicias.js']},
     'o-ateneu':{scripts:['js/corpus/o-ateneu.js']},
-    'macunaima':{scripts:['js/corpus/macunaima.js']},
+    'macunaima':{scripts:['js/corpus/macunaima.js','js/corpus/macunaima-tl111.js']},
     'vidas-secas':{scripts:['js/corpus/vidas-secas.js']}
   });
   const inFlight=new Map();
